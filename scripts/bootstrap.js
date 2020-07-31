52e665ec1374880fed9177d809f2deface1573ec
Sébastien Loix <sabee77@gmail.com>
2020-07-31 15:46:52 +0200

    Update scripts & dependencies

--- scripts/bootstrap.js
+++ scripts/bootstrap.js
@@ -9,21 +9,30 @@ const {
 } = require("./utils");
 
 const createSymlinks = () => {
+  // packages
+  // const kbni18nSrc = resolveKibanaPath('packages', 'kbn-i18n');
+  // const kbni18nDest = resolvePath('node_modules', '@kbn', 'i18n');
+  // createSymlink(kbni18nSrc, kbni18nDest);
+
+  const kbnConfigSchemaSrc = resolveKibanaPath('packages', 'kbn-config-schema');
+  const kbnConfigSchemaDest = resolvePath('node_modules', '@kbn', 'config-schema');
+  createSymlink(kbnConfigSchemaSrc, kbnConfigSchemaDest);
+
   // test_utils
   const testUtilsSrc = resolveKibanaPath("x-pack", "test_utils");
   const testUtilsDest = resolvePath('src', 'kibana', "x-pack", "test_utils");
   createSymlink(testUtilsSrc, testUtilsDest);
 
   // es_ui_shared
-  createPluginSymlink("es_ui_shared");
+  // createPluginSymlink("es_ui_shared");
 };
 
 const init = async (pluginId, isXpack = false) => {
   // Copy over the plugin folder
-  await copyFolder(
-    getKibanaPluginPath(pluginId, isXpack),
-    getPluginPath(pluginId, isXpack)
-  );
+  // await copyFolder(
+  //   getKibanaPluginPath(pluginId, isXpack),
+  //   getPluginPath(pluginId, isXpack)
+  // );
 
   createSymlinks();
 };
--- scripts/kibana.js
+++ scripts/kibana.js
@@ -1,41 +1,39 @@
-const del = require('del');
+const del = require("del");
 
-const { getKibanaPluginPath, getPluginPath, copyFolder } = require('./utils');
+const { getKibanaPluginPath, getPluginPath, copyFolder } = require("./utils");
 
 /**
  * Copy and replace the changes made on a pluggin to the Kibana repo.
  * @param {string} pluginId The pluggin code to copy in Kibana
  * @param {boolean} isXpack If the pluggin is x-pack or not
  */
-const updateKibana = async (pluginId, isXpack) => {
-  const dest = getKibanaPluginPath(pluginId, isXpack);
+const udpateKibanaPluggin = async (pluginId, isXpack) => {
   const src = getPluginPath(pluginId, isXpack);
-  console.log('About to delete', dest);
+  const dest = getKibanaPluginPath(pluginId, isXpack);
 
   // Delete the Kibana pluggin folder
   del.sync([dest], { force: true });
 
   // Replace it with the changes made on this repo
   copyFolder(src, dest);
-}
+};
 
-const ALLOWED_ACTIONS = ['update'];
+const ALLOWED_ACTIONS = ["update"];
 
 const action = process.argv.slice(-1)[0];
 
-if (!action || !ALLOWED_ACTIONS.includes(action)) {
-    throw new Error(`Wrong action passed.`);
+if (!action || !ALLOWED_ACTIONS.includes(action)) {
+  throw new Error(`Wrong action passed.`);
 }
 
 const runScript = async () => {
-    switch(action) {
-        case 'update':
-            await updateKibana('snapshot_restore', true);
-            break;
-        default:
-            return;
-    }
-}
-
+  switch (action) {
+    case "update":
+      await udpateKibanaPluggin("snapshot_restore", true);
+      break;
+    default:
+      return;
+  }
+};
 
 runScript();
